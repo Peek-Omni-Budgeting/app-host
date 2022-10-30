d5f352e38f2807a8a33088454fe20dc0ad8b71c3
Nathan Peek <nmp14fsu@gmail.com>
2022-10-29 21:54:37 -0400

    Setup webpack and some dependencies

---
+++ remotes.config.js
@@ -0,0 +1,7 @@
+const cdnRoot = process.env.CDN_ROOT || 'http://localhost:3000';
+
+const remotes = {
+
+}
+
+module.exports = remotes;
--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,14 @@
 const path = require('path');
+const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ForkTsCheckerPlugin = require('fork-ts-checker-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+const remotes = require('./remotes.config');
+const deps = require('./package.json').dependencies;
 
 const publicPath = `${process.env.PUBLIC_PATH}/`
 
@@ -37,7 +41,96 @@ module.exports = {
       new CssMinimizerPlugin(),
     ]
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {transpileOnly: true},
+        }
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ]
+          }
+        }
+      }, 
+      {
+        test: /\.(c|sc)ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        type: 'asset/resourse',
+        generator: {
+          filename: 'static/images/[contenthash][ext][query]'
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'static/fonts/[contenthash][ext]',
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader'
+      }
+    ]
+  },
+
   plugins: [
-    new MiniCssExtractPlugin(),
-  ]
+    new ModuleFederationPlugin({
+      name: 'app-host',
+      filename: 'remoteEntry.js',
+      remotes: {...remotes},
+      exposes: {},
+      shared: {
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: deps['react-dom'],
+        },
+        'react-router-dom': {
+          singleton: true,
+          requiredVersion: deps['react-router-dom'],
+        },
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[name].[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new AddAssetHtmlPlugin([
+      {
+        filepath: path.resolve(__dirname, './public/styles.css'),
+        hash: true,
+        typeOfAsset: 'css',
+      },
+    ]),
+    new ForkTsCheckerPlugin(),
+    new Dotenv(),
+  ],
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json']
+  }
 };
